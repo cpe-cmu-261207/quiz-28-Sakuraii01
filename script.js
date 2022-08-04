69ba4a477ad9507e1c125fa84859defcb941be96
sakuraii <pearl.k.sakura@gmail.com>
2022-08-04 14:12:34 +0700

    ;-;

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const profile = document.querySelector("#div-user-card")
 const img = document.querySelector("#img-profile")
 const btnRandom = document.querySelector("#btn-random")
+const name = document.getElementById("p-name");
+const bottom = doc.querySelector("#div-loading-card")
 
 btnRandom.onclick = async () =>{
-  profile.style.display = "none";
+  profile.style.display = "";
+  bottom.style.display = "none";
+  
   btnRandom.disabled = true
 
   const resp = await axios.get("https://randomuser.me/api/")
   img.src = resp.data;
-  
+  name.src = resp.data.name;
 }
 
 img.onload = () =>{
   btnRandom.disabled = false
-  profile.style.display = "";
+  profile.style.display = "none";
+  bottom.style.display = "";
   setTimeout(()=>{
       img.className = ""
 },1000)
 }
+
 // 
 
