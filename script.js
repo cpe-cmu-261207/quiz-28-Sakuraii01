96a86a404d0cc5be0331e4555ca0db2b118309e6
sakuraii <pearl.k.sakura@gmail.com>
2022-08-04 14:08:30 +0700

    name and readme

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-async function callApi() {
-  const resp = await axios.get("https://randomuser.me/api/");
+const profile = document.querySelector("#div-user-card")
+const img = document.querySelector("#img-profile")
+const btnRandom = document.querySelector("#btn-random")
+
+btnRandom.onclick = async () =>{
+  profile.style.display = "none";
+  btnRandom.disabled = true
+
+  const resp = await axios.get("https://randomuser.me/api/")
+  img.src = resp.data;
+  
 }
+
+img.onload = () =>{
+  btnRandom.disabled = false
+  profile.style.display = "";
+  setTimeout(()=>{
+      img.className = ""
+},1000)
+}
+// 
+
